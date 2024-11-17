52e6b0576b6a86563a9fb74c10a94e74ac260551
Hassan Ali <ihasanalyy@gmail.com>
2024-11-17 14:15:42 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data.js';
 
 function App() {
+  console.log(data); // Check if data is logging correctly
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      
+      {/* Update prop name to 'recipes' */}
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        padding: '20px',
+        backgroundColor: "black"
+      }}>
+      <Menu recipes={data} />
+      <Menu recipes={data} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const Menu = ({ recipes }) => {
+  return (
+    <div style={{
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "black",
+    
+    }}>
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+      }}
+    >
+      {recipes?.filter((dish) => dish.stock)?.map((dish, index) => (
+        // Always provide a unique key for mapped elements
+        <RecipeMenu dish={dish} key={index} />
+      ))}
+    </div>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "white",
+        color: "black",
+      }}
+    >
+      <h1
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Hotel
+      </h1>
+      <h5
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Take away & Home delivery
+      </h5>
+    </div>
+  );
+};
+
+
+function RecipeMenu({ dish }) {
+  console.log(dish);
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "50%",
+        backgroundColor: "#1c1816",
+        border: "none",
+        borderRadius: "10px",
+        margin: "10px",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white"
+      }}
+    >
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+        }}
+      >
+        <img
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+          src={dish.img}
+          alt={dish.title} // Add alt text for accessibility
+        />
+      </div>
+      <div style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+      }}>
+        <h1>{dish.title}</h1>
+        <p style={{
+          fontSize: "14px",
+          lineHeight: "1.5em",
+          fontFamily: "monospace"
+        }}>{dish.description}</p>
+        <button>{dish.stock ? "ADD TO BUCKET" : "Out of Stock"}</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,59 @@
+const data = [
+    {
+        id: 1,
+        img: "https://www.kfcpakistan.com/images/b0c33f70-bc52-11ee-b144-5b816f8c83f0-Riceandspice_variant_0-2024-01-26135623.png",
+        title: "Rice & Spice",
+        description: "Spiced and buttery rice with 6 pcs of Hot Shots topped with our signature Vietnamese sauce",
+        stock: 2
+    },
+    {
+        id: 2,
+        img: "https://www.kfcpakistan.com/images/b438e990-bc23-11ee-be0d-ed0e61ce8a3a-Untitleddesign(5)-min_variant_0-2024-01-26082002.png",
+        title: "Krunch Burger",
+        description: "Krunch fillet, spicy mayo, lettuce, sandwiched between a sesame seed bun",
+        stock: 6
+    },
+    {
+        id: 3,
+        img: "https://www.kfcpakistan.com/images/3fda5fc0-c1c8-11ee-88d6-317ad09e56fd-Chicken-Strips_variant_0-2024-02-02124030.png",
+        title: "Boneless Strips",
+        description: "3 Chicken strips for a hassle free boneless experience",
+        stock: 3
+    },
+    {
+        id: 4,
+        img: "https://www.kfcpakistan.com/images/637c7f50-6441-11ef-b796-4dbca6679c6f-Thumbnail(2)-2024-08-27065547.png",
+        title: "Krunch Chicken Deal",
+        description: "1 Krunch burger + 1 pc of Hot and Crispy Fried Chicken + 1 Regular drink",
+        stock: 8
+    },
+    {
+        id: 5,
+        img: "https://www.kfcpakistan.com/images/43aa4970-ffaa-11ed-9c55-3705f003c528-CrispyBox-2023-05-31115706.png",
+        title: "Crispy Box",
+        description: "In mood for something crispy? Get 2 pcs Hot & Crispy Chicken + 1 Regular fries + 1 Regular drink + 1 Coleslaw",
+        stock: 5
+    },
+    {
+        id: 6,
+        img: "https://www.kfcpakistan.com/images/43a9fb50-ffaa-11ed-8180-812e571998fe-family-Festivle-3-2023-05-31115706.png",
+        title: "Family Festival 3",
+        description: "An ultimate meal for the fam. It includes 4 Zinger burgers + 4 pieces Hot and Crispy Chicken + 2 Dinner rolls + 1.5 Liter drink",
+        stock: 1
+    },
+    {
+        id: 7,
+        img: "https://www.kfcpakistan.com/images/8d046c40-bc10-11ee-97ff-6b0386c23ddc-Salsa-Wings-min_variant_0-2024-01-26060256.png",
+        title: "Salsa Sprinkle Wings",
+        description: "8 Pcs Salsa Sprinkle Wings Bucket",
+        stock: 4
+    },
+    {
+        id: 8,
+        img: "https://www.kfcpakistan.com/images/43a95f10-ffaa-11ed-b673-4121381f04c6-mighty-Combo-2023-05-31115706.png",
+        title: "Mighty Zinger Combo",
+        description: "Mighty Zinger+ 1 Regular fries + 1 Regular drink",
+        stock: 3
+    }
+]
+export default data
